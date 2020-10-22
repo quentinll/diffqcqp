30c15b17a73e4ca3dba8cfb3dc372b1ccaec0946
quentin <quentin.lelidec@orange.fr>
2020-10-22 19:00:32 +0200

    remove folders2

--- build/qcqplib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/quentin/Documents/willow/diffqcqp/qcqplib
-# Build directory: /home/quentin/Documents/willow/diffqcqp/build/qcqplib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
